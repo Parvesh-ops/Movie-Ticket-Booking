49b63e953d943e5f96100b2887e63590824bbf09
Parvesh <arveshchaudhary@gmail.com>
2026-01-05 12:00:40 +0545

    working on admin Addshows

--- Movie_Ticket_Booking/src/dashboard/AddShows.jsx
+++ Movie_Ticket_Booking/src/dashboard/AddShows.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { dummyShowsData } from '../assets/assets'
 
 const AddShows = () => {
+  const currency = import.meta.env.VITE_CURRENCY
+  const [nowPlayingMovies, setnowPlayingMovies] = useState([])
+  const [selectedMovie, setSelectedMovie] = useState(null)
+  const [dateTimeSelection, setDateTimeSelection] = useState({})
+  const [dateTimeInput, setDateTimeInput] = useState('')
+  const [showPrice, setShowPrice] = useState()
+
+  const fetchNowPlayingMovies = async () => {
+    setnowPlayingMovies(dummyShowsData)
+  }
+
+  useEffect(() => {
+   fetchNowPlayingMovies()
+  }, [])
+  
   return (
     <div>
-     <h1>Add shows</h1>
+      <h1>Add shows</h1>
     </div>
   )
 }
